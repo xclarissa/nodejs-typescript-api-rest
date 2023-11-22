246637320a80949346d51407b68e9211eefe4b9d
Clarissa Xavier <clarissa.xavier@nagem.com.br>
2023-11-22 16:17:00 -0300

    fix: fixing error 14 line

--- src/server/database/providers/pessoa/index.ts
+++ src/server/database/providers/pessoa/index.ts
@@ -3,7 +3,7 @@ import * as getById from './GetById';
 import * as create from './Create';
 import * as update from './Update';
 import * as deleteById from './Delete';
-// import * as count from './Count';
+import * as count from './Count';
 
 export const CidadesProvider = {
   ...getAll,
