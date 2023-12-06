4f14a0a01ff17b281747feea213380e81c484bab
Clarissa Xavier <clarissa.xavier@nagem.com.br>
2023-12-06 18:12:23 -0300

    feat: config cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,11 +3,21 @@ import 'dotenv/config';
 import { JSONParseError } from './shared/middleware';
 import './shared/services/TranslationsYup';
 import { router } from './routes';
+import cors from 'cors';
 
 const server = express();
+
+server.use(cors({
+  origin: process.env.ENABLED_CORS?.split(';') || []
+}));
+// fetch('http://localhost:3333/cidades')
+// .then(data => data.json())
+// .then(console.log)
+// .catch(console.log)
 
 server.use(express.json());
 server.use(JSONParseError);
 server.use(router);
 
 export { server };
+
